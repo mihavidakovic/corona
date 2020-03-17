d870aef0d851b82bc43d3c69c22e6248197819f3
Myha <miha@vidakovic.si>
2020-03-17 20:02:53 +0100

    test

--- client/src/App.js
+++ client/src/App.js
@@ -5,23 +5,12 @@ import logo from './logo.svg';
 import './App.css';
 
 
-function getConfirmed() {
-
-	let json = csvToJson.getJsonFromCsv("confirmed.csv");
-	for(let i=0; i<json.length;i++){
-	    console.log(json[i]);
-	}
-
-}
-
 function App() {
 
-	getConfirmed();
 
 	return (
 		<div className="App">
 			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
 				<p>
 					CORONAAAA
 				</p>
