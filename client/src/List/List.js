b96af274885c7979611db6719e114514239e4845
Myha <miha@vidakovic.si>
2020-03-18 14:43:48 +0100

    design fix

--- client/src/List/List.js
+++ client/src/List/List.js
@@ -59,14 +59,14 @@ export default class List extends React.Component {
 				<div>
 					<div className="list">
 						<div className="list__head">
-							<div>Država</div>
+							<div><span>Država</span></div>
 							<div onClick={this.onSortChange}>
 								<span>Smrti</span>
 								<i className={`fas fa-${sortTypes[currentSort].class}`} />
 							</div>
-							<div>Potrjenih primerov</div>
-							<div>Okrevanih pacientov</div>
-							<div>Posodobljeno</div>
+							<div><span>Potrjenih primerov</span></div>
+							<div><span>Okrevanih pacientov</span></div>
+							<div><span>Posodobljeno</span></div>
 						</div>
 					</div>
 					<div className="loading"></div>
@@ -76,25 +76,26 @@ export default class List extends React.Component {
 			return (
 				<div className="list">
 					<div className="list__head">
-						<div>Država</div>
+						<div><span>Država</span></div>
 						<div onClick={this.onSortChange}>
 							<span>Smrti</span>
 							<i className={`fas fa-${sortTypes[currentSort].class}`} />
 						</div>
-						<div>Potrjenih primerov</div>
-						<div>Okrevanih pacientov</div>
-						<div>Posodobljeno</div>
+						<div><span>Potrjenih primerov</span></div>
+						<div><span>Okrevanih pacientov</span></div>
+						<div><span>Posodobljeno</span></div>
 					</div>
 					<div className="items">
 						{[...this.state.items].sort(sortTypes[currentSort].fn).map(item => 
 							<div className="item">
 								<div className="item__cell item__state">
-									<span>{item.region}</span> {item.state.length > 0 && <span>/ {item.state}</span>}
+									<span>{item.region}</span> 
+									{item.state.length > 0 && <span>{item.state}</span>}
 								</div>
 								<div className="item__cell item__deaths">{item.deaths}</div>
 								<div className="item__cell item__confirmed">{item.confirmed}</div>
 								<div className="item__cell item__recovered">{item.recovered}</div>
-								<div className="item__cell item__date"><Moment fromNow locale="sl">{item.last_update}</Moment></div>
+								<div className="item__cell item__date" hidden title={item.last_update}><Moment fromNow locale="sl">{item.last_update}</Moment></div>
 							</div>
 						)}
 					</div>
