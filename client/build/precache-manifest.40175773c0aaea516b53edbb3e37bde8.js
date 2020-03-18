63b9b5fdc1dd4e3801ed8b9a29691c42170e1868
root <root@vidakovic.si>
2020-03-18 01:37:11 +0000

    jao

---
+++ client/build/precache-manifest.40175773c0aaea516b53edbb3e37bde8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c54a87df6575fc013ee91d05342881f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "751cbcd8a3b1fa738a9c",
+    "url": "/static/css/main.9e6c371d.chunk.css"
+  },
+  {
+    "revision": "1ed61529af7872d1ed93",
+    "url": "/static/js/2.3eb53716.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.3eb53716.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "751cbcd8a3b1fa738a9c",
+    "url": "/static/js/main.88b2dcbe.chunk.js"
+  },
+  {
+    "revision": "09eb2fae383ea5eae762",
+    "url": "/static/js/runtime-main.074d3413.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
