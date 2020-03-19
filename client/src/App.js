4dc43491478d6ca84a80ffa8feacf9d037275e6a
Myha <miha@vidakovic.si>
2020-03-19 02:26:59 +0100

    added preferred color scheme

--- client/src/App.js
+++ client/src/App.js
@@ -8,13 +8,34 @@ import Graph from './Graph/Graph.js';
 
 function App() {
 	const [darkMode, setDarkMode] = React.useState(getInitialMode());
+
+
 	React.useEffect(() => {
 		localStorage.setItem('dark', JSON.stringify(darkMode));
 	}, [darkMode]);
 
 	function getInitialMode() {
+		const isReturningUser = "dark" in localStorage;
 		const savedMode = JSON.parse(localStorage.getItem('dark'));
+		const userPrefersDark = getPrefColorScheme();
+		// if mode was saved -> dark / light
+		if (isReturningUser) {
+			return savedMode;
+		} else if (userPrefersDark) {
+			return true;
+		} else {
+			return false;
+		}
+		// if preferred color scheme dark -> dark
+		// otherwise -> light
+
 		return savedMode || false;
+	}
+
+	function getPrefColorScheme() {
+		if (!window.matchMedia) return;
+
+		return window.matchMedia("(prefers-color-scheme: dark)").matches;
 	}
 
 	return (
