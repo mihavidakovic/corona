8ebac0a2aac207eb6c837c9ae424342681e5ecfb
Myha <miha@vidakovic.si>
2020-03-22 04:12:03 +0100

    data update

--- client/src/App.js
+++ client/src/App.js
@@ -64,7 +64,7 @@ function App() {
 			<section>
 				<header className="section__header">
 					<h3>Podatki</h3>
-					<p className="data__update">Posodobljeno 12.3.2020 02:26</p>
+					<p className="data__update">Posodobljeno 22.3.2020 02:26</p>
 				</header>
 				<List />
 			</section>
