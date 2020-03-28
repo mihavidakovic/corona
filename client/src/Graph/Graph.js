72c5ec0908cb2267eface9ad707e23f29d08dd83
Myha <miha@vidakovic.si>
2020-03-29 00:41:55 +0100

    added graph deaths

--- client/src/Graph/Graph.js
+++ client/src/Graph/Graph.js
@@ -11,7 +11,9 @@ const tooltip = {
 	padding: '0.5rem 1rem',
 	borderRadius: '3px',
 	color: 'black',
-	fontSize: '0.9rem'
+	fontSize: '0.9rem',
+	display: 'flex',
+	flexFlow: 'column'
 };
 
 
@@ -20,7 +22,8 @@ const CustomTooltip = ({ active, payload, label }) => {
   if (active) {
     return (
       <div style={tooltip}>
-        <span className="label">{`${labelFormated}:`} <b>{`${payload[0].value}`} potrjenih primerov</b></span>
+        <span className="label">{`${labelFormated}:`} <b>{`${payload[1].value}`} potrjenih primerov</b></span>
+        <span className="label">{`${labelFormated}:`} <b>{`${payload[0].value}`} smrti</b></span>
       </div>
     );
   }
@@ -28,6 +31,15 @@ const CustomTooltip = ({ active, payload, label }) => {
   return null;
 };
 
+const getPercent = (value, total) => {
+	const ratio = total > 0 ? value / total : 0;
+  
+  return toPercent(ratio, 2);
+};
+
+const toPercent = (decimal, fixed = 0) => {
+	return `${(decimal * 100).toFixed(fixed)}%`;
+};
 
 export default class Graph extends React.Component {
 	state = {
@@ -48,14 +60,28 @@ export default class Graph extends React.Component {
 
 
 	componentWillMount() {
-		axios.get("https://corona.lmao.ninja/v2/historical/slovenia")
+		axios.get('https://corona.lmao.ninja/v2/historical/slovenia')
 		.then(res => {
 			const getData = res.data;
 			const casesKeys = Object.keys(res.data.timeline.cases);
-			const casesValues = Object.values(res.data.timeline.cases);
-			var cases = Object.entries(res.data.timeline.cases).map(([date, Smrti]) => ({date,Smrti}));
+			const deathsValues = Object.values(res.data.timeline.deaths);
+			var cases = Object.entries(res.data.timeline.cases).map(([date, Primerov]) => ({date,Primerov}));
+
+			for (var key in cases) {
+			    // skip loop if the property is from prototype
+			    if (!cases.hasOwnProperty(key)) continue;
+
+			    var obj = cases[key];
+			    Object.assign(obj, {smrti: deathsValues[key]})
+
+			}
+
+		    for (var b = 0; b < cases[0].length; b++) {
 
 
+		    }
+			    console.log(cases)
+	    	
 			this.setState({data: res.data});
 			this.setState({cases: cases});
 			this.setState({casesDates: casesKeys});
@@ -78,11 +104,22 @@ export default class Graph extends React.Component {
 							top: 0, right: 25, left: -15, bottom: 0,
 							}}
 						>
+						  <defs>
+						    <linearGradient id="colorSmrti" x1="0" y1="0" x2="0" y2="1">
+						      <stop offset="5%" stopColor="red" stopOpacity={0.3}/>
+						      <stop offset="95%" stopColor="red" stopOpacity={0}/>
+						    </linearGradient>
+						    <linearGradient id="colorPrimerov" x1="0" y1="0" x2="0" y2="1">
+						      <stop offset="5%" stopColor="rgba(255, 255, 255, 0.3)" stopOpacity={1}/>
+						      <stop offset="95%" stopColor="rgba(255, 255, 255, 1)" stopOpacity={0}/>
+						    </linearGradient>
+						  </defs>
 							<CartesianGrid stroke='rgba(255, 255, 255, 0.2)'/>
-							<XAxis dataKey="date" stroke='rgba(255, 255, 255, 0)' />
+							<XAxis dataKey="date" stroke='rgba(255, 255, 255, 0)' tickFormatter={toPercent} />
 							<YAxis stroke='rgba(255, 255, 255, 0.2)' />
 	        				<Tooltip content={<CustomTooltip />} />
-							<Area type="monotone" dataKey="Smrti" stroke="rgba(255, 255, 255, 1)" fill="rgba(255, 255, 255, 0.7)" />
+							<Area type="monotoneX" isAnimationActive={false}  dataKey="smrti" stroke="rgba(239, 57, 57, 0.8)" fill="rgba(239, 57, 57, 0.8)" fillOpacity={1} fill="url(#colorSmrti)" />
+							<Area type="monotoneX" isAnimationActive={false}  dataKey="Primerov" stroke="rgba(255, 255, 255, 1)" fill="rgba(255, 255, 255, 0.7)" fillOpacity={1} fill="url(#colorPrimerov)" />
 						</AreaChart>
 					</ResponsiveContainer>
 				</div>
