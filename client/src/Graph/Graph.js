59b469c1a70a072379d86c7900e2de6c386fc3e0
Myha <miha@vidakovic.si>
2020-03-28 22:13:25 +0100

    fixed margins

--- client/src/Graph/Graph.js
+++ client/src/Graph/Graph.js
@@ -67,7 +67,7 @@ export default class Graph extends React.Component {
 		return (
 		<>
 				<header className="section__header">
-					<h3>Graf potrjenih primerov v Sloveniji</h3>
+					<h2>Graf potrjenih primerov v Sloveniji</h2>
 				</header>
 				<div style={{width: '100%', height: 300}}>
 					<ResponsiveContainer>
