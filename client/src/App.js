0f5b90b9904fe19a7606a754333d75a5ba7bdb03
Myha <miha@vidakovic.si>
2020-03-29 09:50:24 +0200

    added ip

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import Moment from 'react-moment';
 import 'moment-timezone';
 
@@ -18,6 +19,17 @@ class App extends React.Component {
 		this.state = {
 			darkMode: true,
 		};
+	}
+
+	componentWillMount() {
+		this.getIp();
+	}
+
+	getIp() {
+		axios.get("https://corona.vidakovic.si/api/ip")
+			.then(res => {
+				console.log(res.data);
+		})
 	}
 
 
