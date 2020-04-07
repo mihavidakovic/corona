e6f07474305ca926c161c8a5292780677c62aeb1
Myha <miha@vidakovic.si>
2020-04-07 18:38:34 +0200

    donate

--- client/src/Components/Donate.js
+++ client/src/Components/Donate.js
@@ -21,10 +21,11 @@ class Donate extends React.Component {
 		return (
 			<div className="donate">
 		      <PayPalButton
-		        amount="5.00"
+		        amount="2.00"
 		        // shippingPreference="NO_SHIPPING" // default is "GET_FROM_FILE"
 		        options={{
 		          clientId: "45DUVZZZUDFFC",
+		          currency: "EUR"
 		        }}
 		      />
 			</div>
